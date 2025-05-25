118084edd914ff70b3f93391f85f98a495f491c6
Danush09 <136923185+Danush09@users.noreply.github.com>
2025-05-25 13:49:22 +0530

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://millitary-asset-managment-system.onrender.com/api',
     headers: {
         'Content-Type': 'application/json'
     }
 });
+
 
 // Add a request interceptor to add the auth token to requests
 instance.interceptors.request.use(
